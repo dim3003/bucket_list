b8cba24673b07400a0ed28a010fce570174b00fc
Jey3003 <andre.d3003@gmail.com>
2020-07-01 15:45:08 +0200

    photo url to Idea Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200701131126) do
+ActiveRecord::Schema.define(version: 20200701134432) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "done_count"
+    t.string "photo_url"
   end
 
 end
