569497646c0615683785f8d1e0955ec2d9124a85
Jey3003 <andre.d3003@gmail.com>
2020-07-08 22:43:09 +0200

    test in simple navigation

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -10,6 +10,21 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
   end
 
   test 'style guide navigation' do
-    
+    visit('/')
+    sleep(3.seconds)
+    click_on('Style Guide', match: :first)
+    sleep(3.seconds)
+    assert page.has_content?('Atoms')
+    assert page.has_content?('Molecules')
+    assert page.has_content?('Organisms')
+    sleep(3.seconds)
+    click_on('Molecules', match: :first)
+    assert page.has_content?('Card')
+    sleep(3.seconds)
+    click_on('Organisms', match: :first)
+    assert page.has_content?('Create Goal')
+    sleep(3.seconds)
+    click_on('My Bucket List', match: :first)
+    assert page.has_content?('Some ideas')
   end
 end
