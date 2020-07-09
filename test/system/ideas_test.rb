2900fcfe013d44b05d37da005f19e3d3eb747838
Jey3003 <andre.d3003@gmail.com>
2020-07-09 14:53:39 +0200

    test create new idea

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -5,8 +5,11 @@ class IdeasTest < ApplicationSystemTestCase
     visit('/ideas/new')
     sleep(4.seconds)
     fill_in('title', with: 'Test Idea')
-    fill_in('done_count', with: '420')
+    fill_in('done_count', with: 420)
     fill_in('photo_url', with: 'https://www.gettyimages.co.uk/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg')
+    sleep(2.seconds)
+    click_on('Create idea', match: :first)
+    assert page.has_content?('Test Idea')
     sleep(4.seconds)
   end
 end
