117aea3d07558f68eb35f6f4d3cf54182d0b63d6
Jey3003 <andre.d3003@gmail.com>
2020-07-11 22:00:22 +0200

    class method most_recent in Idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,8 @@ class Idea < ApplicationRecord
   def self.search(search_term)
     where('title LIKE ?', "%#{search_term}%")
   end
+
+  def self.most_recent
+    all.order(created_at: :asc).limit(3)
+  end
 end
