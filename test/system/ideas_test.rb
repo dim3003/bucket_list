c45d442264f3ef28cbc817ce5f96aab844092d23
Jey3003 <andre.d3003@gmail.com>
2020-07-11 23:12:25 +0200

    generate attribute description for Idea2

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -59,4 +59,20 @@ class IdeasTest < ApplicationSystemTestCase
     assert page.has_content?('No idea found')
   end
 
+  test 'check homepage ideas' do
+    4.times do |i|
+      idea = Idea.new
+      idea.title = "Exciting new idea #{i+1}"
+      idea.save!
+    end
+
+    visit('/')
+
+    assert page.has_content?('Exciting new idea 2')
+    assert page.has_content?('Exciting new idea 3')
+    assert page.has_content?('Exciting new idea 4')
+
+    refute page.has_content?('Exciting new idea 1')
+  end
+
 end
