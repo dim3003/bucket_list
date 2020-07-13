45e09afdc90f267657152d133fddcdec62ff7bc6
Jey3003 <andre.d3003@gmail.com>
2020-07-13 18:01:35 +0200

    test adding a comment to an idea

---
+++ test/system/idea_comments_test.rb
@@ -0,0 +1,15 @@
+require "application_system_test_case"
+
+class IdeaCommentsTest < ApplicationSystemTestCase
+
+  test 'adding a Comment to an Idea' do
+    idea = Idea.new title: 'a valid idea'
+    idea.save!
+    visit(idea_path(idea))
+    fill_in('Add a comment', with: 'a comment')
+    click_on('Post', match: :first)
+
+    assert_equal page.current_path, idea_path(idea)
+  end
+
+end
