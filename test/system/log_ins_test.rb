066a9ec81c3532043916aeb89f72d6db7217a4e6
Jey3003 <andre.d3003@gmail.com>
2020-07-14 17:24:13 +0200

    test log in no new user when already signed up

--- test/system/log_ins_test.rb
+++ test/system/log_ins_test.rb
@@ -10,4 +10,13 @@ class LogInsTest < ApplicationSystemTestCase
       assert_equal 'abogus@epfl.ch', User.first.email
   end
 
+  test 'log in does not create a user' do
+    user = User.new email: 'abogus@email.com'
+    user.save!
+    visit(new_user_path)
+    fill_in('Email', with: user.email)
+    click_on('Log in', match: :first)
+    assert_equal 1, User.all.length
+  end
+
 end
