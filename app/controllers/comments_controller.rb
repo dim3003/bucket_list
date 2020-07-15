1fe0f6ec951716d7cb3cd38961abbedfc04bbbb9
Jey3003 <andre.d3003@gmail.com>
2020-07-15 11:18:03 +0200

    comments create controller fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
+    user = User.find(session[:user_id])
     comment = Comment.new(comment_resource_params)
     idea = Idea.find(params[:idea_id])
     comment.idea = idea
-    comment.user = session[:user_id]
+    comment.user = user
     comment.save
     redirect_to idea_path(idea)
   end
--- test/system/idea_comments_test.rb
+++ test/system/idea_comments_test.rb
@@ -12,13 +12,11 @@ class IdeaCommentsTest < ApplicationSystemTestCase
     visit(new_user_path)
     fill_in('Email address', with: user.email)
     click_on('Log in', match: :first)
-    
+
     visit(idea_path(idea))
     fill_in('Add a comment', with: 'a comment')
-    sleep(3.seconds)
     click_on('Post', match: :first)
 
-    sleep (5.seconds)
     assert_equal idea_path(idea), page.current_path
     assert page.has_content?('a comment')
   end
