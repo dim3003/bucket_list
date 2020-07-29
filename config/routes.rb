4dbe9a3d63cc7782def75bc2b8659f540600a9be
Jey3003 <andre.d3003@gmail.com>
2020-07-29 18:13:16 +0200

    flash css

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'account/ideas'
 
 
-  resources :sessions, only[:new, :create]
+  resources :sessions, only: [:new, :create]
 
   resources :users do
     resources :goals
