b823a7eda88d7766daaefd9bfe0fcac5063a6901
Jey3003 <andre.d3003@gmail.com>
2020-07-30 10:38:56 +0200

    email to lowercase before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  before_validation :downcase_email
   validates :email, uniqueness: true
-  
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
 end
