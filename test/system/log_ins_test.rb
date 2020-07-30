98ed5e780a41c2a46c5f58c49130faef7246be3d
Jey3003 <andre.d3003@gmail.com>
2020-07-30 09:50:15 +0200

    fix sign in test in logins test system

--- test/system/log_ins_test.rb
+++ test/system/log_ins_test.rb
@@ -2,10 +2,11 @@ require "application_system_test_case"
 
 class LogInsTest < ApplicationSystemTestCase
 
-  test 'sign up and creates a User' do
+  test 'sign up creates a User' do
       visit(new_user_path)
       fill_in('Email', with: 'abogus@epfl.ch')
-      click_on('Log in', match: :first)
+      fill_in('Password', with: 'password')
+      click_on('Sign In', match: :first)
       assert_equal 1, User.all.length
       assert_equal 'abogus@epfl.ch', User.first.email
   end
