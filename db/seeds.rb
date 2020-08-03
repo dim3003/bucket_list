191bd10e416554fc99c01b926f80470d31273fc4
Jey3003 <andre.d3003@gmail.com>
2020-08-03 19:40:47 +0200

    create ideas in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 admin = User.create! email: 'neill@extensionschool.ch',
                      password: 'password',
                      name: 'NJ Pearman',
@@ -22,3 +15,10 @@ user2 = User.create! email: 'visitor2@extensionschool.ch',
 user3 = User.create! email: 'visitor3@extensionschool.ch',
                      password: 'password',
                      name: 'Visitor'
+
+1.upto(5) do |i|
+  Idea.create! title: "Idea for #{admin.name} #{i}", user: admin
+  Idea.create! title: "Idea for #{user1.name} #{i}", user: user1
+  Idea.create! title: "Idea for #{user2.name} #{i}", user: user2
+  Idea.create! title: "Idea for #{user3.name} #{i}", user: user3
+end
