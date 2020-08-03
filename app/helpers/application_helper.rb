3cdb2522893b233187f693e62fcf131ced02a687
Jey3003 <andre.d3003@gmail.com>
2020-08-03 16:40:37 +0200

    roles helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
 
-  def can_edit?(goal)
-    case(current_user.role)
-      when 'admin' then true
-      when 'registered' then current_user == goal.user
-      else false
-    end
-  end
-
 end
---
+++ app/helpers/roles_helper.rb
@@ -0,0 +1,11 @@
+module RolesHelper
+
+  def can_edit?(goal)
+    case(current_user.role)
+      when 'admin' then true
+      when 'registered' then current_user == goal.user
+      else false
+    end
+  end
+
+end
