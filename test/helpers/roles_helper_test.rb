e42753745f7d93c0a0fb64abe6ed19dfa8b37200
Jey3003 <andre.d3003@gmail.com>
2020-08-03 17:44:34 +0200

    user helper test

--- test/helpers/roles_helper_test.rb
+++ test/helpers/roles_helper_test.rb
@@ -4,10 +4,19 @@ class RolesHelperTest < ActionView::TestCase
   attr_accessor :current_user
 
   test 'registered users can edit owned ideas' do
-    user = User.create(email: 'test@epfl.ch')
+    user = User.create(email: 'owner@epfl.ch')
     self.current_user = user
 
     idea = Idea.create(title: 'A test idea!', user: user)
+
+    assert can_edit?(idea)
+  end
+
+  test 'admin user can edit not owned ideas' do
+    user = User.create(email: 'admin@epfl.ch', role: 'admin')
+    self.current_user = user
+
+    idea = Idea.create(title: 'A test idea!', user: User.new)
 
     assert can_edit?(idea)
   end
