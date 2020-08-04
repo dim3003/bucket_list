02d21d672e36c4e511990f885d3570b3048ae542
Jey3003 <andre.d3003@gmail.com>
2020-08-04 10:47:56 +0200

    comment controller create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,11 @@ class CommentsController < ApplicationController
   before_action :ensure_authenticated
 
   def create
-    comment = Comment.new(comment_resource_params)
+    @comment = Comment.new(comment_resource_params)
     idea = Idea.find(params[:idea_id])
-    comment.idea = idea
-    comment.user = current_user
-    comment.save
-    redirect_to idea_path(idea)
+    @comment.idea = idea
+    @comment.user = current_user
+    @comment.save
   end
 
   private
