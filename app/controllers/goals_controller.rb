b4079a267296e2fb6c060d6fd65b9bd518893403
Jey3003 <andre.d3003@gmail.com>
2020-08-04 12:00:54 +0200

    different format goal create

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,7 +5,10 @@ class GoalsController < ApplicationController
     idea = Idea.find(params[:idea_id])
     current_user.goals << idea
 
-    redirect_to idea_path(idea)
+    respond_to do |format|
+      format.html { redirect_to idea_path(idea) }
+      format.js
+    end
   end
 
 end
